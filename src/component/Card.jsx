ba489c5430e688f4f11cd7a7cf62122c5a354861
leeap <Ssenge1515@gmail.com>
2024-01-11 18:58:06 +0800

    class 1-10

---
+++ src/component/Card.jsx
@@ -0,0 +1,6 @@
+export const Card = () => {
+    <div className="container">
+        <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW5RnBJC9QoG1hUJ-FzScrElTGe9er9w8pWw&usqp=CAU" alt="" />
+    </div>
+} 
+
